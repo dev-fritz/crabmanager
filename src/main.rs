a32b093908327115bfd4cdcb12eaa617ea8c8512
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-10-19 19:09:33 -0400

    feat: update main file to execute the api with db connection and log

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,58 @@
-mod models;
-mod handlers;
-mod services;
 mod config;
+mod handlers;
+mod models;
 mod repositories;
+mod services;
+
+use config::database::establish_connection;
 
 use actix_cors::Cors;
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{http::header, web, App, HttpServer};
+use dotenvy::dotenv;
+use log::{info, error};
 
-#[get("/")]
-async fn index() -> impl Responder {
-    format!("Hello world!")
+struct _AppState {
+    _db: sqlx::MySqlPool,
 }
 
 #[actix_web::main]
-async fn main () -> Result<(), std::io::Error> {
-    
-    HttpServer::new(|| {
-        
+async fn main() -> Result<(), std::io::Error> {
+    dotenv().ok();
+    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+
+    info!("Logging system initialized successfully!"); // Testando se o log funciona
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    info!("Database URL: {}", database_url); // Log de informação
+
+    let pool = match establish_connection(database_url).await {
+        Ok(pool) => pool,
+        Err(e) => {
+            error!("Failed to connect to database: {}", e); // Log de erro
+            return Err(std::io::Error::new(std::io::ErrorKind::Other, "Failed to connect to database"));
+        }
+    };
+
+    // Inicializa o servidor HTTP
+    HttpServer::new(move || {
         let cors = Cors::default()
-            .allow_any_origin()
-            .allow_any_method()
-            .allow_any_header();
-        
+            .allowed_origin("http://localhost:3000")
+            .allowed_methods(vec!["GET", "POST", "PUT","PATCH", "DELETE"])
+            .allowed_headers(vec![
+                header::CONTENT_TYPE,
+                header::AUTHORIZATION,
+                header::ACCEPT,
+            ])
+            .supports_credentials();
+
         App::new()
+            .app_data(web::Data::new(_AppState { _db: pool.clone() }))
+            .configure(handlers::config)
             .wrap(cors)
-            .service(index)
-        
+            .wrap(actix_web::middleware::Logger::default())
     })
-        
-    .bind(("127.0.0.1", 8080)).unwrap()
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
+
