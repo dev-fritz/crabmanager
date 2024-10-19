b2e956272a89c1c45368b8287783364444c22cc7
Fritz Henrique <fritzhenrique.dev@gmail.com>
2024-10-19 19:07:50 -0400

    feat: update database file to create db pool

--- src/config/database.rs
+++ src/config/database.rs
@@ -1,11 +1,12 @@
 use sqlx::mysql::{MySqlPoolOptions, MySqlPool};
 
-
-/// Establish connection to the database
-pub async fn establish_connection() -> Result<MySqlPool, sqlx::Error> {
+pub async fn establish_connection(database_url: String) -> Result<MySqlPool, sqlx::Error> {
     let pool = MySqlPoolOptions::new()
-        .max_connections(5)
-        .connect("mysql://root:password@localhost:3306/db_name").await;
-    pool
-}
+        .max_connections(10)
+        .idle_timeout(std::time::Duration::from_secs(10))
+        .acquire_timeout(std::time::Duration::from_secs(30))
+        .connect(&database_url)
+        .await?;
 
+    Ok(pool)
+}
